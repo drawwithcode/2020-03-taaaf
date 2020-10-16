9ded1f3e00e35e99bbf0f4c2950d018e5a59c5c5
taaaf <mattia.tafel@gmail.com>
2020-10-16 13:08:20 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,203 @@
-function preload(){
-  // put preload code here
+let mySong;
+
+let dataDeaths;
+
+let myPersonalCorpses = [];
+
+
+
+function preload() {
+
+  mySong = loadSound("./assets/music/rain.mp3");
+
+  dataDeaths = loadJSON("VSRR_Provisional_Drug_Overdose_Death_Counts.json");
+
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+  console.log(mySong);
+
+  mySong.loop();
+
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySong);
+
+  console.log(dataDeaths.deaths.length);
+
+
+  console.log(myPersonalCorpses.lenght);
+
+
+  addCorpse();
+
+
 }
 
 function draw() {
-  // put drawing code here
+
+
+  background(255);
+
+
+  let volume = 0;
+
+  volume = analyzer.getLevel();
+
+  volume = map(volume, 0 , 1 , 0 , 0.005);
+
+
+  for (let i = 0; i < myPersonalCorpses.length; i++) {
+
+    translate(0,volume);
+
+    myPersonalCorpses[i].run();
+
+  }
+
+
+  console.log(volume);
+
+
+}
+
+
+function windowResized() {
+
+
+  resizeCanvas(windowWidth, windowHeight);
+
+  // for(let i = 0; i < myPersonalCorpses.length; i++){
+  //
+  //   myPersonalCorpses[i]=0;
+  //
+  //
+  // }
+
+
+  addCorpse();
+
+
+
+
+}
+
+let i = 0;
+
+function mouseClicked() {
+
+  if (i == 0) {
+
+    mySong.play();
+
+  }
+
+  i++;
+
+  background(255);
+}
+
+let x,y;
+
+let a = 0;
+
+function addCorpse() {
+
+
+
+console.log(dataDeaths.deaths.length);
+
+// if(a==0){
+
+  for (let i = 0; i < dataDeaths.deaths.length; i++) {
+
+    if(dataDeaths.deaths[i].Indicator == "Number of Drug Overdose Deaths" && dataDeaths.deaths[i].State !="US"){
+
+      let size = map(dataDeaths.deaths[i]["Data Value"], 0, 3000, 0, 50);
+
+      // let size = dataDeaths.deaths[i]["Data Value"];
+
+
+      console.log(size);
+
+      const newCorpse = new Corpse(random(0,width),random(0,height),size/2,random(1,200));
+
+      myPersonalCorpses.push(newCorpse);
+
+
+
+    }
+
+
+}
+//
+// }else{
+//
+//
+//   for (let i = 0; i < dataDeaths.deaths.length; i++) {
+//
+//     if(dataDeaths.deaths[i].Indicator == "Number of Drug Overdose Deaths" && dataDeaths.deaths[i].State !="US"){
+//
+//       let size = map(dataDeaths.deaths[i]["Data Value"], 0, 3000, 0, 50);
+//
+//       // let size = dataDeaths.deaths[i]["Data Value"];
+//
+//
+//       console.log(size);
+//
+//       const newCorpse = new Corpse(random(0,width),random(0,height),size/2,random(1,200));
+//
+//       myPersonalCorpses[i]=newCorpse;
+//
+//
+//
+//
+//
+//
+//
+//
+// }
+//
+//
+// a++;
+
+
+
+
+  }
+
+
+
+
+class Corpse {
+
+  constructor(x, y, radius, color) {
+
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+
+  }
+
+
+  display() {
+
+    push();
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.radius * 2);
+    pop();
+
+  }
+
+
+  run() {
+
+      this.display();
+
+
+  }
+
 }
