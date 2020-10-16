171456d821d48e44d9d0db6faa6893879c4a38b7
taaaf <mattia.tafel@gmail.com>
2020-10-16 18:59:34 +0200

    final code

--- sketch.js
+++ sketch.js
@@ -1,9 +1,6 @@
 let mySong;
-
 let dataDeaths;
-
 let myPersonalCorpses = [];
-
 
 
 function preload() {
@@ -14,57 +11,75 @@ function preload() {
 
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
 
-  console.log(mySong);
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+  //
+  // console.log(mySong);
 
   mySong.loop();
 
   analyzer = new p5.Amplitude();
   analyzer.setInput(mySong);
 
-  console.log(dataDeaths.deaths.length);
-
-
-  console.log(myPersonalCorpses.lenght);
-
+  // console.log(dataDeaths.deaths.length);
+  //
+  //
+  // console.log(myPersonalCorpses.lenght);
 
   addCorpse();
 
-
 }
+
 
 function draw() {
 
-
   background(255);
-
 
   let volume = 0;
 
   volume = analyzer.getLevel();
 
-  volume = map(volume, 0 , 1 , 0 , 0.005);
+  volume = map(volume, 0, 1, 0, 0.008);
 
+push();
 
   for (let i = 0; i < myPersonalCorpses.length; i++) {
 
-    translate(0,volume);
+    translate(0, volume);
 
     myPersonalCorpses[i].run();
 
   }
 
+  pop();
 
-  console.log(volume);
+  push()
+  noStroke();
+  fill(255, 255, 255,180);
+  rect(0, 0, 210, 65);
+  pop();
 
+  push()
+  noStroke();
+  fill(0);
+  ellipse(20, 20, 33);
+  pop();
+
+  text("= 1000 Drug Overdose Deaths", 40,23);
+  text("(USA from 2015 to 2020)", 50,33);
+
+  text("click to make them dance",33,55);
+
+
+  // console.log(volume);
+  //
 
 }
 
 
 function windowResized() {
-
 
   resizeCanvas(windowWidth, windowHeight);
 
@@ -75,13 +90,19 @@ function windowResized() {
   //
   // }
 
+  for (let i = myPersonalCorpses.length - 1; i >= 0; i--) {
+
+    myPersonalCorpses.splice(i, 1);
+
+    // console.log(myPersonalCorpses[i]);
+
+  }
 
   addCorpse();
 
-
-
-
 }
+
+
 
 let i = 0;
 
@@ -95,78 +116,72 @@ function mouseClicked() {
 
   i++;
 
-  background(255);
 }
 
-let x,y;
 
-let a = 0;
+
+let x, y;
+//
+// let a = 0;
 
 function addCorpse() {
 
+  //
+  //
+  // console.log(dataDeaths.deaths.length);
 
-
-console.log(dataDeaths.deaths.length);
-
-// if(a==0){
+  // if(a==0){
 
   for (let i = 0; i < dataDeaths.deaths.length; i++) {
 
-    if(dataDeaths.deaths[i].Indicator == "Number of Drug Overdose Deaths" && dataDeaths.deaths[i].State !="US"){
+    if (dataDeaths.deaths[i].Indicator == "Number of Drug Overdose Deaths" && dataDeaths.deaths[i].State != "US") {
 
-      let size = map(dataDeaths.deaths[i]["Data Value"], 0, 3000, 0, 50);
+      let size = dataDeaths.deaths[i]["Data Value"] / 60;
 
       // let size = dataDeaths.deaths[i]["Data Value"];
 
+      //
+      // console.log(size);
 
-      console.log(size);
-
-      const newCorpse = new Corpse(random(0,width),random(0,height),size/2,random(1,200));
+      const newCorpse = new Corpse(random(0, width), random(0, height), size / 2, random(1, 200));
 
       myPersonalCorpses.push(newCorpse);
 
-
-
     }
 
+  }
+  //
+  // }else{
+  //
+  //
+  //   for (let i = 0; i < dataDeaths.deaths.length; i++) {
+  //
+  //     if(dataDeaths.deaths[i].Indicator == "Number of Drug Overdose Deaths" && dataDeaths.deaths[i].State !="US"){
+  //
+  //       let size = map(dataDeaths.deaths[i]["Data Value"], 0, 3000, 0, 50);
+  //
+  //       // let size = dataDeaths.deaths[i]["Data Value"];
+  //
+  //
+  //       console.log(size);
+  //
+  //       const newCorpse = new Corpse(random(0,width),random(0,height),size/2,random(1,200));
+  //
+  //       myPersonalCorpses[i]=newCorpse;
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  // }
+  //
+  //
+  // a++;
 
 }
-//
-// }else{
-//
-//
-//   for (let i = 0; i < dataDeaths.deaths.length; i++) {
-//
-//     if(dataDeaths.deaths[i].Indicator == "Number of Drug Overdose Deaths" && dataDeaths.deaths[i].State !="US"){
-//
-//       let size = map(dataDeaths.deaths[i]["Data Value"], 0, 3000, 0, 50);
-//
-//       // let size = dataDeaths.deaths[i]["Data Value"];
-//
-//
-//       console.log(size);
-//
-//       const newCorpse = new Corpse(random(0,width),random(0,height),size/2,random(1,200));
-//
-//       myPersonalCorpses[i]=newCorpse;
-//
-//
-//
-//
-//
-//
-//
-//
-// }
-//
-//
-// a++;
-
-
-
-
-  }
-
 
 
 
@@ -181,22 +196,20 @@ class Corpse {
 
   }
 
-
   display() {
 
     push();
     noStroke();
     fill(this.color);
+    translate(random(0, this.radius) / 20, random(0, this.radius) / 20);
     ellipse(this.x, this.y, this.radius * 2);
     pop();
 
   }
 
-
   run() {
 
-      this.display();
-
+    this.display();
 
   }
 
